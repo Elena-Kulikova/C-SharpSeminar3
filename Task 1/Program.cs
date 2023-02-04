fe77a0e0afbcc54b4ae7930df1a0f7c69d43ca61
ElenaKulikova <elena_komb@mail.ru>
2023-02-04 13:16:50 +0200

    Added solution to Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -4,3 +4,22 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"Число {number} - является палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} - не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"Введеное число не является пятизначным");
+}
